f1f371cc03ef33687e3b39cd020da4907ed86dc9
Jiaxin Shou <shoujiaxin@gmail.com>
2020-01-07 13:06:41 +0800

    Solve 169. Majority Element

---
+++ 169. Majority Element/solution1.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=169 lang=cpp
+ *
+ * [169] Majority Element
+ *
+ * https://leetcode.com/problems/majority-element/description/
+ *
+ * algorithms
+ * Easy (54.51%)
+ * Likes:    2295
+ * Dislikes: 192
+ * Total Accepted:    483.7K
+ * Total Submissions: 873.8K
+ * Testcase Example:  '[3,2,3]'
+ *
+ * Given an array of size n, find the majority element. The majority element is
+ * the element that appears more than ⌊ n/2 ⌋ times.
+ *
+ * You may assume that the array is non-empty and the majority element always
+ * exist in the array.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [3,2,3]
+ * Output: 3
+ *
+ * Example 2:
+ *
+ *
+ * Input: [2,2,1,1,1,2,2]
+ * Output: 2
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int majorityElement(vector<int>& nums) {
+    std::map<int, int> numCounter;
+    for (const auto& n : nums) {
+      ++numCounter[n];
+    }
+
+    for (const auto& counter : numCounter) {
+      if (counter.second > nums.size() / 2) {
+        return counter.first;
+      }
+    }
+
+    return 0;
+  }
+};
+// @lc code=end
