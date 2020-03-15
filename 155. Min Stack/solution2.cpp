e459e910edf88a08a335157552c86c6a5ef04d0c
Jiaxin Shou <shoujiaxin@gmail.com>
2020-03-15 15:57:45 +0800

    Add solution of 155

---
+++ 155. Min Stack/solution2.cpp
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=155 lang=cpp
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (41.17%)
+ * Likes:    2709
+ * Dislikes: 279
+ * Total Accepted:    422K
+ * Total Submissions: 1M
+ * Testcase Example:
+ '["MinStack","push","push","push","getMin","pop","top","getMin"]\n' +
+  '[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ *
+ *
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ *
+ *
+ *
+ *
+ * Example:
+ *
+ *
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ *
+ *
+ *
+ *
+ */
+
+// @lc code=start
+class MinStack {
+ public:
+  /** initialize your data structure here. */
+  MinStack() {}
+
+  void push(int x) {
+    elements.push(x);
+
+    if (mins.empty() || x <= mins.top()) {
+      mins.push(x);
+    }
+  }
+
+  void pop() {
+    auto num = elements.top();
+    elements.pop();
+
+    if (mins.top() == num) {
+      mins.pop();
+    }
+  }
+
+  int top() { return elements.top(); }
+
+  int getMin() { return mins.top(); }
+
+ private:
+  stack<int> elements;
+  stack<int> mins;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+// @lc code=end
