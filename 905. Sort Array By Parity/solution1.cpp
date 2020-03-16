1973143b337bee3eac22f8552e6bf8f1215adf3d
Jiaxin Shou <shoujiaxin@gmail.com>
2020-03-17 01:13:29 +0800

    Solve 905. Sort Array By Parity

---
+++ 905. Sort Array By Parity/solution1.cpp
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=905 lang=cpp
+ *
+ * [905] Sort Array By Parity
+ *
+ * https://leetcode.com/problems/sort-array-by-parity/description/
+ *
+ * algorithms
+ * Easy (73.71%)
+ * Likes:    762
+ * Dislikes: 69
+ * Total Accepted:    163.5K
+ * Total Submissions: 221.8K
+ * Testcase Example:  '[3,1,2,4]'
+ *
+ * Given an array A of non-negative integers, return an array consisting of all
+ * the even elements of A, followed by all the odd elements of A.
+ *
+ * You may return any answer array that satisfies this condition.
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: [3,1,2,4]
+ * Output: [2,4,3,1]
+ * The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * 1 <= A.length <= 5000
+ * 0 <= A[i] <= 5000
+ *
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  vector<int> sortArrayByParity(vector<int>& A) {
+    if (A.empty()) {
+      return A;
+    }
+
+    auto left = A.begin();
+    auto right = A.end() - 1;
+
+    while (left < right) {
+      while (left < right && *left % 2 == 0) {
+        ++left;
+      }
+      while (left < right && *right % 2 == 1) {
+        --right;
+      }
+      swap(*left, *right);
+      ++left;
+      --right;
+    }
+
+    return A;
+  }
+};
+// @lc code=end
