1d1f420b87e1bc95ce5390b655be8884fa62caaf
Jiaxin Shou <shoujiaxin@gmail.com>
2020-03-20 21:59:24 +0800

    Solve 62. Unique Paths

---
+++ 62. Unique Paths/solution1.cpp
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=62 lang=cpp
+ *
+ * [62] Unique Paths
+ *
+ * https://leetcode.com/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (51.47%)
+ * Likes:    2503
+ * Dislikes: 180
+ * Total Accepted:    404.5K
+ * Total Submissions: 785.2K
+ * Testcase Example:  '3\n2'
+ *
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in
+ * the diagram below).
+ *
+ * The robot can only move either down or right at any point in time. The robot
+ * is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+ * the diagram below).
+ *
+ * How many possible unique paths are there?
+ *
+ *
+ * Above is a 7 x 3 grid. How many possible unique paths are there?
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: m = 3, n = 2
+ * Output: 3
+ * Explanation:
+ * From the top-left corner, there are a total of 3 ways to reach the
+ * bottom-right corner:
+ * 1. Right -> Right -> Down
+ * 2. Right -> Down -> Right
+ * 3. Down -> Right -> Right
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: m = 7, n = 3
+ * Output: 28
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= m, n <= 100
+ * It's guaranteed that the answer will be less than or equal to 2 * 10 ^ 9.
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int uniquePaths(int m, int n) {
+    vector<vector<long>> dp(m, vector<long>(n, 1));
+
+    for (int i = 1; i < m; ++i) {
+      for (int j = 1; j < n; ++j) {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+
+    return dp[m - 1][n - 1];
+  }
+};
+// @lc code=end
