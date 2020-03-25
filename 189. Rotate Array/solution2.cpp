3d1f020aafdec4551108e276072997ad4062004f
Jiaxin Shou <shoujiaxin@gmail.com>
2020-03-26 01:36:01 +0800

    Solve 189. Rotate Array

---
+++ 189. Rotate Array/solution2.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=189 lang=cpp
+ *
+ * [189] Rotate Array
+ *
+ * https://leetcode.com/problems/rotate-array/description/
+ *
+ * algorithms
+ * Easy (33.15%)
+ * Likes:    2272
+ * Dislikes: 755
+ * Total Accepted:    425.1K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,2,3,4,5,6,7]\n3'
+ *
+ * Given an array, rotate the array to the right by k steps, where k is
+ * non-negative.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ *
+ *
+ * Note:
+ *
+ *
+ * Try to come up as many solutions as you can, there are at least 3 different
+ * ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ *
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  void rotate(vector<int>& nums, int k) {
+    k %= nums.size();
+    // 三次旋转交换两个序列
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+  }
+};
+// @lc code=end
