746d30312c9b7be58e9eff83b4961928cccf7ba8
Jiaxin Shou <shoujiaxin@gmail.com>
2020-04-14 00:39:15 +0800

    Solve 290. Word Pattern

---
+++ 290. Word Pattern/solution1.cpp
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=290 lang=cpp
+ *
+ * [290] Word Pattern
+ *
+ * https://leetcode.com/problems/word-pattern/description/
+ *
+ * algorithms
+ * Easy (36.44%)
+ * Likes:    940
+ * Dislikes: 131
+ * Total Accepted:    177.2K
+ * Total Submissions: 485.9K
+ * Testcase Example:  '"abba"\n"dog cat cat dog"'
+ *
+ * Given a pattern and a string str, find if str follows the same pattern.
+ *
+ * Here follow means a full match, such that there is a bijection between a
+ * letter in pattern and a non-empty word in str.
+ *
+ * Example 1:
+ *
+ *
+ * Input: pattern = "abba", str = "dog cat cat dog"
+ * Output: true
+ *
+ * Example 2:
+ *
+ *
+ * Input:pattern = "abba", str = "dog cat cat fish"
+ * Output: false
+ *
+ * Example 3:
+ *
+ *
+ * Input: pattern = "aaaa", str = "dog cat cat dog"
+ * Output: false
+ *
+ * Example 4:
+ *
+ *
+ * Input: pattern = "abba", str = "dog dog dog dog"
+ * Output: false
+ *
+ * Notes:
+ * You may assume pattern contains only lowercase letters, and str contains
+ * lowercase letters that may be separated by a single space.
+ *
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  bool wordPattern(string pattern, string str) {
+    unordered_map<char, string> p2s;
+    unordered_map<string, char> s2p;
+
+    istringstream istm(str);
+    string word;
+
+    for (const auto& c : pattern) {
+      if (!(istm >> word)) {
+        return false;
+      }
+
+      if (p2s.find(c) == p2s.end() && s2p.find(word) == s2p.end()) {
+        p2s[c] = word;
+        s2p[word] = c;
+      } else if (p2s[c] != word || s2p[word] != c) {
+        return false;
+      }
+    }
+
+    return !(istm >> word);
+  }
+};
+// @lc code=end
