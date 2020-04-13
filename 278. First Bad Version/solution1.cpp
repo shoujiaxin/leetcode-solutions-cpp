12b056c59d2787b08ab9ed91f41f9cad7ef86e1d
Jiaxin Shou <shoujiaxin@gmail.com>
2020-04-14 00:18:13 +0800

    Solve 278. First Bad Version

---
+++ 278. First Bad Version/solution1.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=278 lang=cpp
+ *
+ * [278] First Bad Version
+ *
+ * https://leetcode.com/problems/first-bad-version/description/
+ *
+ * algorithms
+ * Easy (33.47%)
+ * Likes:    1060
+ * Dislikes: 580
+ * Total Accepted:    314.7K
+ * Total Submissions: 938.3K
+ * Testcase Example:  '5\n4'
+ *
+ * You are a product manager and currently leading a team to develop a new
+ * product. Unfortunately, the latest version of your product fails the quality
+ * check. Since each version is developed based on the previous version, all
+ * the versions after a bad version are also bad.
+ *
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the
+ * first bad one, which causes all the following ones to be bad.
+ *
+ * You are given an API bool isBadVersion(version) which will return whether
+ * version is bad. Implement a function to find the first bad version. You
+ * should minimize the number of calls to the API.
+ *
+ * Example:
+ *
+ *
+ * Given n = 5, and version = 4 is the first bad version.
+ *
+ * call isBadVersion(3) -> false
+ * call isBadVersion(5) -> true
+ * call isBadVersion(4) -> true
+ *
+ * Then 4 is the first bad version. 
+ *
+ *
+ */
+
+// @lc code=start
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+ public:
+  int firstBadVersion(int n) {
+    long first = 1;
+    long last = n;
+
+    while (first < last) {
+      long mid = (first + last) / 2;
+
+      isBadVersion(mid) ? last = mid : first = mid + 1;
+    }
+
+    return first;
+  }
+};
+// @lc code=end
