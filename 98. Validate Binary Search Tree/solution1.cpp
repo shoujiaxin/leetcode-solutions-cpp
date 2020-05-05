da6ccf7002a89ea75cd0d7101919bea00e01f8b8
martinshou <martinshou@tencent.com>
2020-05-05 19:00:41 +0800

    Solve 98. Validate Binary Search Tree

---
+++ 98. Validate Binary Search Tree/solution1.cpp
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode id=98 lang=cpp
+ *
+ * [98] Validate Binary Search Tree
+ *
+ * https://leetcode.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (27.36%)
+ * Likes:    3411
+ * Dislikes: 487
+ * Total Accepted:    636.1K
+ * Total Submissions: 2.3M
+ * Testcase Example:  '[2,1,3]'
+ *
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ *
+ * Assume a BST is defined as follows:
+ *
+ *
+ * The left subtree of a node contains only nodes with keys less than the
+ * node's key.
+ * The right subtree of a node contains only nodes with keys greater than the
+ * node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ *
+ * Input: [2,1,3]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 1   4
+ * / \
+ * 3   6
+ *
+ * Input: [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is 4.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  bool isValidBST(TreeNode* root) {
+    if (root == nullptr) {
+      return true;
+    }
+
+    // 中序遍历，当前节点一定大于上一个节点
+    if (!isValidBST(root->left)) {
+      return false;
+    }
+
+    if (root->val <= pre) {
+      return false;
+    }
+    pre = root->val;
+
+    return isValidBST(root->right);
+  }
+
+ private:
+  long pre = LONG_MIN;  // 中序遍历的上一个节点
+};
+// @lc code=end
