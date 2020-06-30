7e353b54b1932a0a58d08bb80c6c2608839773ac
Jiaxin Shou <shoujiaxin@gmail.com>
2020-06-30 20:03:06 +0800

    Add a solution of 110

---
+++ 110. Balanced Binary Tree/solution2.cpp
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode id=110 lang=cpp
+ *
+ * [110] Balanced Binary Tree
+ *
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (42.91%)
+ * Likes:    2176
+ * Dislikes: 165
+ * Total Accepted:    444K
+ * Total Submissions: 1M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, determine if it is height-balanced.
+ *
+ * For this problem, a height-balanced binary tree is defined as:
+ *
+ *
+ * a binary tree in which the left and right subtrees of every node differ in
+ * height by no more than 1.
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Given the following tree [3,9,20,null,null,15,7]:
+ *
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ * Return true.
+ *
+ * Example 2:
+ *
+ * Given the following tree [1,2,2,3,3,null,null,4,4]:
+ *
+ *
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   2
+ * ⁠   / \
+ * ⁠  3   3
+ * ⁠ / \
+ * ⁠4   4
+ *
+ *
+ * Return false.
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  bool isBalanced(TreeNode* root) { return height(root) != -1; }
+
+ private:
+  int height(TreeNode* root) {
+    if (root == nullptr) {
+      return 0;
+    }
+
+    // 剪枝
+    auto leftHeight = height(root->left);
+    if (leftHeight == -1) {
+      return -1;
+    }
+
+    auto rightHeight = height(root->right);
+    if (rightHeight == -1) {
+      return -1;
+    }
+
+    if (abs(leftHeight - rightHeight) > 1) {
+      return -1;
+    }
+
+    return max(leftHeight, rightHeight) + 1;
+  }
+};
+// @lc code=end
