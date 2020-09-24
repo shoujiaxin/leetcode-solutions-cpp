feda58b388ba7e5b3f3b2821c15f8e51ada6b2e8
Jiaxin Shou <shoujiaxin@gmail.com>
2020-09-24 10:26:56 +0800

    Solve 501. Find Mode in Binary Search Tree

---
+++ solution2.cpp
@@ -0,0 +1,103 @@
+/*
+ * @lc app=leetcode id=501 lang=cpp
+ *
+ * [501] Find Mode in Binary Search Tree
+ *
+ * https://leetcode.com/problems/find-mode-in-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (42.03%)
+ * Likes:    991
+ * Dislikes: 366
+ * Total Accepted:    91.7K
+ * Total Submissions: 215K
+ * Testcase Example:  '[1,null,2,2]'
+ *
+ * Given a binary search tree (BST) with duplicates, find all the mode(s) (the
+ * most frequently occurred element) in the given BST.
+ *
+ * Assume a BST is defined as follows:
+ *
+ *
+ * The left subtree of a node contains only nodes with keys less than or equal
+ * to the node's key.
+ * The right subtree of a node contains only nodes with keys greater than or
+ * equal to the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ *
+ *
+ *
+ *
+ * For example:
+ * Given BST [1,null,2,2],
+ *
+ *
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  2
+ *
+ *
+ *
+ *
+ * return [2].
+ *
+ * Note: If a tree has more than one mode, you can return them in any order.
+ *
+ * Follow up: Could you do that without using any extra space? (Assume that the
+ * implicit stack space incurred due to recursion does not count).
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  vector<int> findMode(TreeNode* root) {
+    dfs(root);
+
+    return res;
+  }
+
+ private:
+  int curNum = 1;           // 当前数字个数
+  int maxNum = 1;           // 最大个数
+  TreeNode* pre = nullptr;  // 前一个节点
+  vector<int> res;
+
+  void dfs(TreeNode* root) {
+    if (root == nullptr) {
+      return;
+    }
+
+    dfs(root->left);
+
+    if (pre) {
+      curNum = (pre->val == root->val) ? curNum + 1 : 1;
+    }
+
+    if (curNum == maxNum) {
+      res.push_back(root->val);
+    } else if (curNum > maxNum) {
+      res.clear();
+      res.push_back(root->val);
+      maxNum = curNum;
+    }
+
+    pre = root;
+    dfs(root->right);
+  }
+};
+// @lc code=end
