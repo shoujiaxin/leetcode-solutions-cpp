a7f7eb831a5ddeccdc9f7b0a9f65c9f227add836
Jiaxin Shou <shoujiaxin@gmail.com>
2021-02-17 13:34:31 +0800

    Solve 566. Reshape the Matrix

---
+++ 566. Reshape the Matrix/solution1.cpp
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=566 lang=cpp
+ *
+ * [566] Reshape the Matrix
+ *
+ * https://leetcode.com/problems/reshape-the-matrix/description/
+ *
+ * algorithms
+ * Easy (60.99%)
+ * Likes:    939
+ * Dislikes: 108
+ * Total Accepted:    112.7K
+ * Total Submissions: 184.6K
+ * Testcase Example:  '[[1,2],[3,4]]\n1\n4'
+ *
+ * In MATLAB, there is a very useful function called 'reshape', which can
+ * reshape a matrix into a new one with different size but keep its original
+ * data.
+ *
+ *
+ *
+ * You're given a matrix represented by a two-dimensional array, and two
+ * positive integers r and c representing the row number and column number of
+ * the wanted reshaped matrix, respectively.
+ *
+ * ⁠The reshaped matrix need to be filled with all the elements of the
+ * original matrix in the same row-traversing order as they were.
+ *
+ *
+ *
+ * If the 'reshape' operation with given parameters is possible and legal,
+ * output the new reshaped matrix; Otherwise, output the original matrix.
+ *
+ *
+ * Example 1:
+ *
+ * Input:
+ * nums =
+ * [[1,2],
+ * ⁠[3,4]]
+ * r = 1, c = 4
+ * Output:
+ * [[1,2,3,4]]
+ * Explanation:The row-traversing of nums is [1,2,3,4]. The new reshaped matrix
+ * is a 1 * 4 matrix, fill it row by row by using the previous list.
+ *
+ *
+ *
+ * Example 2:
+ *
+ * Input:
+ * nums =
+ * [[1,2],
+ * ⁠[3,4]]
+ * r = 2, c = 4
+ * Output:
+ * [[1,2],
+ * ⁠[3,4]]
+ * Explanation:There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So
+ * output the original matrix.
+ *
+ *
+ *
+ * Note:
+ *
+ * The height and width of the given matrix is in range [1, 100].
+ * The given r and c are all positive.
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+    if (r * c != nums.size() * nums[0].size()) {
+      return nums;
+    }
+
+    auto res = vector<vector<int>>(r, vector<int>(c, 0));
+    for (auto i = 0; i < r * c; ++i) {
+      res[i / c][i % c] = nums[i / nums[0].size()][i % nums[0].size()];
+    }
+    return res;
+  }
+};
+// @lc code=end
