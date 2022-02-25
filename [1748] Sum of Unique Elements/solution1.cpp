ff99c27918df8debfbe45fc41c47fa31837c883a
Jiaxin Shou <shoujiaxin@gmail.com>
2022-02-25 21:02:35 +0800

    [1748] Sum of Unique Elements

---
+++ [1748] Sum of Unique Elements/solution1.cpp
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=1748 lang=cpp
+ *
+ * [1748] Sum of Unique Elements
+ *
+ * https://leetcode.com/problems/sum-of-unique-elements/description/
+ *
+ * algorithms
+ * Easy (75.20%)
+ * Likes:    665
+ * Dislikes: 17
+ * Total Accepted:    67.2K
+ * Total Submissions: 89.2K
+ * Testcase Example:  '[1,2,3,2]'
+ *
+ * You are given an integer array nums. The unique elements of an array are the
+ * elements that appear exactly once in the array.
+ *
+ * Return the sum of all the unique elements of nums.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [1,2,3,2]
+ * Output: 4
+ * Explanation: The unique elements are [1,3], and the sum is 4.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [1,1,1,1,1]
+ * Output: 0
+ * Explanation: There are no unique elements, and the sum is 0.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums = [1,2,3,4,5]
+ * Output: 15
+ * Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 100
+ * 1 <= nums[i] <= 100
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int sumOfUnique(vector<int>& nums) {
+    unordered_map<int, int> numCounter;
+    for (const auto& n : nums) {
+      ++numCounter[n];
+    }
+
+    auto sum = 0;
+    for (const auto& cnt : numCounter) {
+      if (cnt.second == 1) {
+        sum += cnt.first;
+      }
+    }
+    return sum;
+  }
+};
+// @lc code=end
