70322c3bf2093860c3d6247337fbbdb4cff6eb88
Jiaxin Shou <shoujiaxin@gmail.com>
2022-05-15 17:14:42 +0800

    [304] Range Sum Query 2D - Immutable

--- [304] Range Sum Query 2D - Immutable/solution1.cpp
+++ [304] Range Sum Query 2D - Immutable/solution1.cpp
@@ -6,45 +6,64 @@
  * https://leetcode.com/problems/range-sum-query-2d-immutable/description/
  *
  * algorithms
- * Medium (38.00%)
- * Likes:    1157
- * Dislikes: 188
- * Total Accepted:    126.1K
- * Total Submissions: 322.1K
+ * Medium (47.76%)
+ * Likes:    2546
+ * Dislikes: 247
+ * Total Accepted:    225K
+ * Total Submissions: 470.4K
  * Testcase Example:  '["NumMatrix","sumRegion","sumRegion","sumRegion"]\n' +
   '[[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]'
  *
- * Given a 2D matrix matrix, find the sum of the elements inside the rectangle
- * defined by its upper left corner (row1, col1) and lower right corner (row2,
- * col2).
+ * Given a 2D matrix matrix, handle multiple queries of the following
+ * type:
+ *
+ *
+ * Calculate the sum of the elements of matrix inside the rectangle defined by
+ * its upper left corner (row1, col1) and lower right corner (row2, col2).
+ *
+ *
+ * Implement the NumMatrix class:
+ *
+ *
+ * NumMatrix(int[][] matrix) Initializes the object with the integer matrix
+ * matrix.
+ * int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the
+ * elements of matrix inside the rectangle defined by its upper left corner
+ * (row1, col1) and lower right corner (row2, col2).
  *
  *
  *
- * The above rectangle (with the red border) is defined by (row1, col1) = (2,
- * 1) and (row2, col2) = (4, 3), which contains sum = 8.
+ * Example 1:
  *
  *
- * Example:
+ * Input
+ * ["NumMatrix", "sumRegion", "sumRegion", "sumRegion"]
+ * [[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1,
+ * 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]]
+ * Output
+ * [null, 8, 11, 12]
  *
- * Given matrix = [
- * ⁠ [3, 0, 1, 4, 2],
- * ⁠ [5, 6, 3, 2, 1],
- * ⁠ [1, 2, 0, 1, 5],
- * ⁠ [4, 1, 0, 1, 7],
- * ⁠ [1, 0, 3, 0, 5]
- * ]
- *
- * sumRegion(2, 1, 4, 3) -> 8
- * sumRegion(1, 1, 2, 2) -> 11
- * sumRegion(1, 2, 2, 4) -> 12
+ * Explanation
+ * NumMatrix numMatrix = new NumMatrix([[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1,
+ * 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]);
+ * numMatrix.sumRegion(2, 1, 4, 3); // return 8 (i.e sum of the red rectangle)
+ * numMatrix.sumRegion(1, 1, 2, 2); // return 11 (i.e sum of the green
+ * rectangle)
+ * numMatrix.sumRegion(1, 2, 2, 4); // return 12 (i.e sum of the blue
+ * rectangle)
  *
  *
  *
- * Note:
+ * Constraints:
  *
- * You may assume that the matrix does not change.
- * There are many calls to sumRegion function.
- * You may assume that row1 ≤ row2 and col1 ≤ col2.
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 200
+ * -10^5 <= matrix[i][j] <= 10^5
+ * 0 <= row1 <= row2 < m
+ * 0 <= col1 <= col2 < n
+ * At most 10^4 calls will be made to sumRegion.
  *
  *
  */
@@ -57,25 +76,24 @@ class NumMatrix {
       return;
     }
 
-    auto rows = matrix.size();
-    auto cols = matrix[0].size();
-    vector<vector<int>> temp(rows, vector<int>(cols, 0));
-    temp[0][0] = matrix[0][0];
-    for (auto i = 1; i < rows; ++i) {
-      temp[i][0] = matrix[i][0] + temp[i - 1][0];
+    const auto m = matrix.size();
+    const auto n = matrix[0].size();
+    dp = vector<vector<int>>(m, vector<int>(n, 0));
+
+    dp[0][0] = matrix[0][0];
+    for (auto i = 1; i < m; ++i) {
+      dp[i][0] = matrix[i][0] + dp[i - 1][0];
     }
-    for (auto i = 1; i < cols; ++i) {
-      temp[0][i] += matrix[0][i] + temp[0][i - 1];
+    for (auto i = 1; i < n; ++i) {
+      dp[0][i] = matrix[0][i] + dp[0][i - 1];
     }
 
-    for (auto i = 1; i < rows; ++i) {
-      for (auto j = 1; j < cols; ++j) {
-        temp[i][j] =
-            matrix[i][j] + temp[i - 1][j] + temp[i][j - 1] - temp[i - 1][j - 1];
+    for (auto i = 1; i < m; ++i) {
+      for (auto j = 1; j < n; ++j) {
+        dp[i][j] =
+            matrix[i][j] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
       }
     }
-
-    dp = std::move(temp);
   }
 
   int sumRegion(int row1, int col1, int row2, int col2) {
