c2fea030733520a6426359cd1e7d73958f01a767
Jiaxin Shou <shoujiaxin@gmail.com>
2022-07-31 00:20:26 +0800

    [1161] Maximum Level Sum of a Binary Tree

---
+++ [1161] Maximum Level Sum of a Binary Tree/solution1.cpp
@@ -0,0 +1,101 @@
+/*
+ * @lc app=leetcode id=1161 lang=cpp
+ *
+ * [1161] Maximum Level Sum of a Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/description/
+ *
+ * algorithms
+ * Medium (66.35%)
+ * Likes:    1530
+ * Dislikes: 60
+ * Total Accepted:    110.8K
+ * Total Submissions: 167K
+ * Testcase Example:  '[1,7,0,7,-8,null,null]'
+ *
+ * Given the root of a binary tree, the level of its root is 1, the level of
+ * its children is 2, and so on.
+ *
+ * Return the smallest level x such that the sum of all the values of nodes at
+ * level x is maximal.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,7,0,7,-8,null,null]
+ * Output: 2
+ * Explanation:
+ * Level 1 sum = 1.
+ * Level 2 sum = 7 + 0 = 7.
+ * Level 3 sum = 7 + -8 = -1.
+ * So we return the level with the maximum sum which is level 2.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
+ * Output: 2
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [1, 10^4].
+ * -10^5 <= Node.val <= 10^5
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  int maxLevelSum(TreeNode* root) {
+    auto maxIndex = 1;
+    auto maxSum = root->val;
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    auto index = 1;
+    while (!q.empty()) {
+      const auto n = q.size();
+      auto sum = 0;
+      for (auto i = 0; i < n; ++i) {
+        const auto node = q.front();
+        q.pop();
+
+        sum += node->val;
+        if (node->left) {
+          q.push(node->left);
+        }
+        if (node->right) {
+          q.push(node->right);
+        }
+      }
+
+      if (sum > maxSum) {
+        maxSum = sum;
+        maxIndex = index;
+      }
+      ++index;
+    }
+
+    return maxIndex;
+  }
+};
+// @lc code=end
